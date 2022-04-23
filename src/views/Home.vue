646e958867bdbeedd9f02ffe4fff2bbe8ef7946c
dorothymea <meao0505@gmail.com>
2022-04-23 14:02:08 +0800

    home页面样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,9 +5,9 @@
     <div class="banner">
       <h1>轮子UI</h1>
       <h2>一个UI框架</h2>
-      <p class="action">
-        <a href="">github</a>
-        <a href="">start</a>
+      <p class="actions">
+        <a href="https://github.com">github</a>
+          <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -23,5 +23,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
