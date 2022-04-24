db554ef1b2506137270d7864955f55ac3afebcd0
dorothymea <meao0505@gmail.com>
2022-04-24 23:30:46 +0800

    Dialog支持visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,25 @@
 <template>
   <h2>Dialog示例</h2>
-  <Dialog></Dialog>
+  <Button @click="toggle">点击弹出对话</Button>
+  <Dialog v-model:visible="x"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from "../lib/Button.vue";
+import {ref} from "vue";
+
 export default {
   components:{
-    Dialog
+    Dialog,
+    Button
+  },
+  setup(){
+    const x = ref(false)
+    const toggle = () =>{
+      x.value = !x.value
+    }
+    return {x, toggle}
   }
 }
 </script>
