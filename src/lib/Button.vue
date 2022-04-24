b392141ad9ef49bfee049c7070bab4d4ea617e61
dorothymea <meao0505@gmail.com>
2022-04-24 22:15:47 +0800

    添加size属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,31 @@
 <template>
-    <button class="ruri-button" :class="{[`ruri-theme-${theme}`]:theme}">
+    <button class="ruri-button" :class="classes">
       <slot/>
     </button>
 </template>
 
 <script lang="ts">
+import {computed} from "vue";
 export default {
   props:{
     theme:{
       type:String,
       default:'button'
+    },
+    size:{
+      type:String,
+      default: 'normal'
     }
+  },
+  setup(props){
+    const { theme, size } = props
+    const classes = computed(()=>{
+      return {
+        [`ruri-theme-${theme}`]:theme,
+        [`ruri-size-${size}`]:size
+      }
+    })
+    return {classes}
   }
 }
 </script>
@@ -35,35 +50,54 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+
   & + & {
     margin-left: 8px;
   }
+
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
+
   &:focus {
     outline: none;
   }
+
   &::-moz-focus-inner {
     border: 0;
   }
-  &.ruri-theme-link{
+
+  &.ruri-theme-link {
     border-color: transparent;
     box-shadow: none;
     color: $blue;
-    &:hover,&:focus{
+
+    &:hover, &:focus {
       color: lighten($blue, 10%);
     }
   }
-  &.ruri-theme-text{
+
+  &.ruri-theme-text {
     border-color: transparent;
     box-shadow: none;
     color: inherit;
-    &:hover,&:focus{
+
+    &:hover, &:focus {
       background: darken(white, 5%);;
     }
   }
+    &.ruri-size-big {
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+
+    &.ruri-size-small {
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
 }
 </style>
