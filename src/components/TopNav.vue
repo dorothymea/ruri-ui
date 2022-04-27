8f9adc258bad46552e2f504f028299fd2939eeb0
dorothymea <meao0505@gmail.com>
2022-04-27 16:48:32 +0800

    logo添加跳转链接

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="topNav">
-    <div class="logo">
+    <router-link to="/" class="logo">
       <svg class="icon">
         <use xlink:href="#icon-crystal-12"></use>
       </svg>
-    </div>
+    </router-link>
     <ul class="menu">
       <li>
         <router-link to="/doc">文档</router-link>
