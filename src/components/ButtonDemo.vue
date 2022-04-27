6ef9030400c32ca70581d9cd396ade26e27aa2a7
dorothymea <meao0505@gmail.com>
2022-04-27 23:25:43 +0800

    Button组件页面 装修

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,66 +1,24 @@
 <template>
-
-    <h2>示例1</h2>
-    <Button theme="button">你好</Button>
-    <Button theme="link">你好</Button>
-    <Button theme="text">你好</Button>
-
-    <h2>示例2</h2>
-    <div>
-      <Button size="big">大大大</Button>
-      <Button>普普通</Button>
-      <Button size="small">小小小</Button>
-    </div>
-    <div>
-      <Button theme="link" size="big">大大大</Button>
-      <Button theme="link">普普通</Button>
-      <Button size="small" theme="link">小小小</Button>
-    </div>
-    <div>
-      <Button size="big" theme="text">大大大</Button>
-      <Button theme="text">普普通</Button>
-      <Button size="small" theme="text">小小小</Button>
-    </div>
-    <h2>示例3</h2>
-    <div>
-      <Button level="main">主要按钮</Button>
-      <Button>普通按钮</Button>
-      <Button level="danger">危险按钮</Button>
-    </div>
-     <div>
-       <Button theme="link" level="main">主要链接按钮</Button>
-       <Button theme="link">普通链接按钮</Button>
-       <Button theme="link" level="danger">危险链接按钮</Button>
-     </div>
-    <div>
-      <Button theme="text" level="main">主要文字按钮</Button>
-      <Button theme="text">普通文字按钮</Button>
-      <Button theme="text" level="danger">危险文字按钮</Button>
-    </div>
-  <h2>示例4</h2>
   <div>
-    <Button disabled>禁用按钮</Button>
-    <Button theme="link" disabled>禁用链接按钮</Button>
-    <Button theme="text" disabled>禁用按钮</Button>
-  </div>
-  <h2>示例5</h2>
-  <div>
-    <Button loading>加载中</Button>
-    <Button>加载完毕</Button>
+    <Content :component="Item1" />
+    <Content :component="Item2" />
+    <Content :component="Item3" />
+    <Content :component="Item4" />
+    <Content :component="Item5" />
   </div>
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import Content from './Content.vue'
+import Item1 from './ButtonItems/Item1.vue'
+import Item2 from './ButtonItems/Item2.vue'
+import Item3 from './ButtonItems/Item3.vue'
+import Item4 from './ButtonItems/Item4.vue'
+import Item5 from './ButtonItems/Item5.vue'
 export default {
-  components:{
-    Button
-  },
+  components:{ Content },
   setup(){
-    const onClick = ()=>{
-      console.log('hi')
-    }
-    return {onClick}
+    return { Item1, Item2, Item3, Item4, Item5 }
   }
 }
 </script>
---
+++ src/components/ButtonItems/Item1.vue
@@ -0,0 +1,15 @@
+<demo>普通按钮</demo>
+
+<template>
+  <Button theme="button">你好</Button>
+  <Button theme="link">你好</Button>
+  <Button theme="text">你好</Button>
+</template>
+
+<script lang="ts">
+import Button from '../../lib/Button.vue'
+
+export default {
+  components:{ Button }
+}
+</script>
