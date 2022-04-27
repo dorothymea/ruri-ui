2a2d9b542e42615a6c3808b905b8a13a10dda079
dorothymea <meao0505@gmail.com>
2022-04-28 00:01:38 +0800

    优化，页面跳转，aside覆盖

--- src/router.ts
+++ src/router.ts
@@ -5,7 +5,6 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import DocDemo from "./components/DocDemo.vue";
 import Intro from "./views/Intro.vue"
 import Install from "./views/Install.vue"
 import GetStarted from "./views/GetStarted.vue"
@@ -27,7 +26,7 @@ export const router = createRouter({
                 { path: "button", component: ButtonDemo },
                 { path: "dialog", component: DialogDemo },
                 { path: "tabs", component: TabsDemo },
-                { path:'',component:DocDemo }
+                { path:'',redirect:'/doc/intro' }
             ]
         }
     ]
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -87,6 +87,7 @@ aside {
   left: 0;
   padding: 70px 0;
   height: 100%;
+  z-index: 10;
   > h2 {
     margin-bottom: 4px;
     padding: 0 16px;
