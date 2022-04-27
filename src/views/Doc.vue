1f73822095c0eebc4b3b869f5ec562b6028f5b9e
dorothymea <meao0505@gmail.com>
2022-04-27 16:58:09 +0800

    创建新路由，Install,Intro,GetStarted

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,6 +3,18 @@
     <TopNav class="nav" toggleAsideButtonVisible/>
     <div class="container">
       <aside v-if="asideVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">准备开始</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
