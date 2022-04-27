18fb8c8b2c035e793850371937240f47bfc4aafa
dorothymea <meao0505@gmail.com>
2022-04-27 19:15:13 +0800

    引入markdown文件

---
+++ src/markdown/intro.md
@@ -0,0 +1,9 @@
+# 介绍
+
+Ruri UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。
+
+此组件库是我学习Vue 3 新特性和 Vue 3 + TypeScript 开发实践期间的项目作品，仅供展示和学习使用。
+
+强烈**不建议**你将此 UI 库用于生产环境。
+
+下一节：[安装](#/doc/install)
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,10 +1,15 @@
 <template>
-  <article class="markdown-body">
-    <h1>介绍</h1>
-    <p>Ruri UI 是基于 Vue 3 和 TypeScript 实现的 UI 组件库。</p>
-    <p>此组件库是我学习Vue 3 新特性和 Vue 3 + TypeScript 开发实践期间的项目作品，仅供展示和学习使用。</p>
-    <p>强烈 <strong>不推荐</strong> 将此UI 库用于生产环境！</p>
-    <p></p>
-    下一节：<a href="#/doc/install">安装</a>
-  </article>
+<article class="markdown-body" v-html="md">
+</article>
 </template>
+
+<script>
+import md from '../markdown/intro.md';
+export default {
+  data() {
+    return {
+      md
+    }
+  }
+}
+</script>
