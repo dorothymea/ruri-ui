bee702a83b62d788fdeff5c1baa16f5a0089ebf1
dorothymea <meao0505@gmail.com>
2022-04-28 13:44:24 +0800

    修复bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -33,6 +33,8 @@ export default {
         const {left:left2} = selectedItem.value.getBoundingClientRect()
         const left = left2 - left1
         indicator.value.style.left = left - 5 + 'px'
+      },{
+        flush: 'post'
       })
     })
 
